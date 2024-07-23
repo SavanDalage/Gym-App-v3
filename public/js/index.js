e62a8d26b22bc4b77fe572953c2d3942156f1e7a
SavanDalage <nekomimiwolf@gmail.com>
2024-07-23 21:51:51 +0200

    about plus działające przyciski

--- public/js/index.js
+++ public/js/index.js
@@ -33,3 +33,33 @@ function createObservers() {
 createObservers();
 // sprawdzanie zmiany wysokości strony
 window.addEventListener("resize", createObservers);
+
+// widzianość stron oraz aktywność przycisków
+
+var buttonsArray = Array.from(document.getElementsByClassName("nav-button"));
+
+var containerArray = Array.from(document.getElementsByClassName("container"));
+
+function hide(index) {
+  containerArray.forEach((container) => {
+    container.classList.remove("visible");
+  });
+  document.getElementById(`${index}`).classList.add("visible");
+}
+
+function highlight(event) {
+  buttonsArray.forEach((button) => {
+    button.classList.remove("active");
+  });
+  event.classList.add("active");
+}
+
+buttonsArray.forEach((button, index) => {
+  button.addEventListener("click", (event) => {
+    var targetButton = event.target.closest(".nav-button");
+    if (targetButton) {
+      highlight(targetButton);
+      hide(index);
+    }
+  });
+});
